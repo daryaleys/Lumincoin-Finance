30cffb5eb0a2787573203d1db46f278b02630161
Darya <daria.lazareva@raketa.agency>
2024-04-26 16:59:20 +0300

    chart js

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -1,15 +1,15 @@
-import jQuery from 'jquery';
-import $ from 'jquery';
+import jQuery from "jquery";
+import $ from "jquery";
 window.jQuery = $;
 window.$ = jQuery;
 
 import "moment";
 // import "moment-with-locales-es6";
 // import "moment/locale/ru.js";
-import 'bootstrap';
-import 'bootstrap-datepicker';
+import "bootstrap";
+import "bootstrap-datepicker";
 
-import '../scss/styles.scss';
-import {Router} from "./router";
+import "../scss/styles.scss";
+import { Router } from "./router";
 
-new Router()
+new Router();
--- frontend/src/js/components/main.js
+++ frontend/src/js/components/main.js
@@ -1,5 +1,75 @@
+import Chart from "chart.js/auto";
+
 export class Main {
-    constructor() {
-        console.log('main')
-    }
+  constructor() {
+    const incomeData = {
+      labels: ["Red", "Orange", "Yellow", "Green", "Blue"],
+      datasets: [
+        {
+          label: "Доходы",
+          data: [300, 50, 100, 25, 75],
+          backgroundColor: [
+            "rgb(220, 53, 69)",
+            "rgb(253, 126, 20)",
+            "rgb(255, 193, 7)",
+            "rgb(32, 201, 151)",
+            "rgb(13, 119, 253)",
+          ],
+          hoverOffset: 4,
+        },
+      ],
+    };
+
+    new Chart(document.getElementById("income-pie"), {
+      type: "pie",
+      data: incomeData,
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            labels: {
+              font: {
+                size: 12,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const expenseData = {
+      labels: ["Red", "Orange", "Yellow", "Green", "Blue"],
+      datasets: [
+        {
+          label: "Расходы",
+          data: [100, 100, 80, 100, 100],
+          backgroundColor: [
+            "rgb(220, 53, 69)",
+            "rgb(253, 126, 20)",
+            "rgb(255, 193, 7)",
+            "rgb(32, 201, 151)",
+            "rgb(13, 119, 253)",
+          ],
+          hoverOffset: 4,
+        },
+      ],
+    };
+
+    new Chart(document.getElementById("expenses-pie"), {
+      type: "pie",
+      data: expenseData,
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            labels: {
+              font: {
+                size: 12,
+              },
+            },
+          },
+        },
+      },
+    });
+  }
 }
