1292d57d596ae31e5d286fdaee97c761f2f263ee
Darya <daria.lazareva@raketa.agency>
2024-05-02 16:29:12 +0300

    create category request

--- frontend/src/js/helpers/requests.js
+++ frontend/src/js/helpers/requests.js
@@ -1,90 +1,129 @@
-import {UserInfo} from "./userInfo";
+import { UserInfo } from "./userInfo";
 
 export class Requests {
-    static host = "http://localhost:3000/api"
-    static headers = {
-        'Content-type': 'application/json',
-        'Accept': 'application/json',
+  static host = "http://localhost:3000/api";
+  static headers = {
+    "Content-type": "application/json",
+    Accept: "application/json",
+  };
+  static authHeaders = {
+    "Content-type": "application/json",
+    Accept: "application/json",
+    "x-auth-token": UserInfo.getUserInfo().accessToken,
+  };
+
+  static async login(body) {
+    const response = await fetch(this.host + "/login", {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify(body),
+    });
+
+    return await response.json();
+  }
+
+  static async register(body) {
+    const response = await fetch(this.host + "/signup", {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify(body),
+    });
+
+    return await response.json();
+  }
+
+  static async logout(body) {
+    const response = await fetch(this.host + "/logout", {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify(body),
+    });
+
+    return await response.json();
+  }
+
+  static async refresh() {
+    const refreshToken = UserInfo.getUserInfo().refreshToken;
+
+    const response = await fetch(this.host + "/refresh", {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify({ refreshToken: refreshToken }),
+    });
+
+    const result = await response.json();
+
+    if (!result || result.error || !result.tokens) {
+      UserInfo.removeUserInfo();
+      result.redirect = "/login";
+    } else {
+      const user = JSON.parse(UserInfo.getUserInfo().userInfo);
+      UserInfo.setUserInfo(
+        result.tokens.accessToken,
+        result.tokens.refreshToken,
+        user.id,
+        user.name,
+        user.lastName
+      );
     }
 
-    static async login(body) {
-        const response = await fetch(this.host + '/login', {
-            method: 'POST',
-            headers: this.headers,
-            body: JSON.stringify(body)
-        })
+    return result;
+  }
 
-        return await response.json();
+  static async getBalance() {
+    const accessToken = UserInfo.getUserInfo().accessToken;
+    if (accessToken) {
+      this.headers["x-auth-token"] = accessToken;
     }
 
-    static async register(body) {
-        const response = await fetch(this.host + '/signup', {
-            method: 'POST',
-            headers: this.headers,
-            body: JSON.stringify(body)
-        })
+    const response = await fetch(this.host + "/balance", {
+      method: "GET",
+      headers: this.headers,
+    });
 
-        return await response.json();
-    }
+    const result = await response.json();
 
-    static async logout(body) {
-        const response = await fetch(this.host + '/logout', {
-            method: 'POST',
-            headers: this.headers,
-            body: JSON.stringify(body)
-        })
-
-        return await response.json();
-    }
-
-    static async refresh() {
-        const refreshToken = UserInfo.getUserInfo().refreshToken;
-
-        const response = await fetch(this.host + '/refresh', {
-            method: 'POST',
-            headers: this.headers,
-            body: JSON.stringify({refreshToken: refreshToken})
-        })
-
-        const result = await response.json()
-
-        if (!result || result.error || !result.tokens) {
-            UserInfo.removeUserInfo();
-            result.redirect = '/login';
-        } else {
-            const user = JSON.parse(UserInfo.getUserInfo().userInfo)
-            UserInfo.setUserInfo(result.tokens.accessToken, result.tokens.refreshToken, user.id, user.name, user.lastName);
+    if (response.status === 401) {
+      // остальные ошибки обрабатываются на месте вызова функции
+      if (!accessToken) {
+        result.redirect = "/login";
+        return result;
+      } else {
+        const refreshResult = await this.refresh();
+        if (refreshResult.redirect) {
+          return refreshResult;
         }
-
-        return result
+        return await this.getBalance();
+      }
     }
 
-    static async getBalance() {
-        const accessToken = UserInfo.getUserInfo().accessToken;
-        if (accessToken) {
-            this.headers["x-auth-token"] = accessToken;
+    return result;
+  }
+
+  static async createCategory(body) {
+    const response = await fetch(this.host + "/categories/income", {
+      method: "POST",
+      headers: this.authHeaders,
+      body: JSON.stringify(body),
+    });
+
+    const result = await response.json();
+    console.log(result);
+
+    if (response.status === 401) {
+      const accessToken = UserInfo.getUserInfo().accessToken;
+      if (!accessToken) {
+        result.redirect = "/login";
+        return result;
+      } else {
+        const refreshResult = await this.refresh();
+        if (refreshResult.redirect) {
+          return refreshResult;
         }
-
-        const response = await fetch(this.host + '/balance', {
-            method: 'GET',
-            headers: this.headers,
-        })
-
-        const result = await response.json();
-
-        if (response.status === 401) { // остальные ошибки обрабатываются на месте вызова функции
-            if (!accessToken) {
-                result.redirect = '/login'
-                return result
-            } else {
-                const refreshResult = await this.refresh();
-                if (refreshResult.redirect) {
-                    return refreshResult
-                }
-                return await this.getBalance()
-            }
-        }
-
-        return result
+        return await this.createCategory(body);
+      }
     }
+
+    return result;
+  }
 }
