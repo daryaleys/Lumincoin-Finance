4f97125d5c10076e8806e4bdef17dc357339e67e
Darya <daria.lazareva@raketa.agency>
2024-05-03 16:43:32 +0300

    operations edit start

--- frontend/src/js/components/operations/operation-edit.js
+++ frontend/src/js/components/operations/operation-edit.js
@@ -1,5 +1,106 @@
+import { Requests } from "../../helpers/requests";
+
 export class OperationEdit {
-  constructor() {
-    console.log("Редактирование дохода/расхода");
+  constructor(openNewRoute) {
+    this.openNewRoute = openNewRoute;
+
+    this.operationTypeElement = document.getElementById("operation-type");
+    this.operationCategoryElement =
+      document.getElementById("operation-category");
+    this.operationAmountElement = document.getElementById("operation-amount");
+    this.operationDateElement = document.getElementById("operation-date");
+    this.operationCommentElement = document.getElementById("operation-comment");
+
+    // this.operationTypeElement.addEventListener(
+    //   "change",
+    //   this.getCategories().bind(this)
+    // );
+
+    let url = new URL(window.location);
+    const operationId = url.searchParams.get("operation");
+    if (!operationId) {
+      return this.openNewRoute("/income-and-expenses");
+    }
+
+    this.getOperation(operationId);
+
+    document
+      .getElementById("edit-operation-button")
+      .addEventListener("click", this.editOperation.bind(this));
+
+    $("#operation-date").datepicker({
+      format: "dd.mm.yyyy",
+      locale: "ru",
+    });
   }
+
+  async getOperation(operationId) {
+    const result = await Requests.request(
+      "/operations/" + operationId,
+      "GET",
+      true
+    );
+
+    if (result.redirect) {
+      this.openNewRoute(result.redirect);
+    }
+
+    if (result.error) {
+      return alert(
+        "Возникла ошибка при запросе операции. Пожалуйста, обратитесь в поддержку"
+      );
+    }
+
+    // this.getCategories();
+
+    this.operationTypeElement.value = result.type;
+    // this.operationCategoryElement.value = result.category;
+    this.operationAmountElement.value = result.amount;
+    this.operationDateElement.value = result.date
+      .split("-")
+      .reverse()
+      .join(".");
+    this.operationCommentElement.value = result.comment;
+
+    console.log(result);
+  }
+
+  async getCategories() {
+    const operationType = e.target.value;
+
+    const result = await Requests.request(
+      "/categories/" + operationType,
+      "GET",
+      true
+    );
+
+    if (result.redirect) {
+      this.openNewRoute(result.redirect);
+    }
+
+    if (result.error) {
+      return alert(
+        "Возникла ошибка при запросе категорий. Пожалуйста, обратитесь в поддержку"
+      );
+    }
+
+    this.addCategoriesToSelect(result);
+  }
+
+  addCategoriesToSelect(categories) {
+    const currentOptions =
+      this.operationCategoryElement.querySelectorAll("option");
+    for (let i = 1; i < currentOptions.length; i++) {
+      currentOptions[i].remove();
+    }
+
+    categories.forEach((category) => {
+      const optionElement = document.createElement("option");
+      optionElement.value = category.id;
+      optionElement.innerText = category.title;
+      this.operationCategoryElement.appendChild(optionElement);
+    });
+  }
+
+  editOperation() {}
 }
--- frontend/src/js/components/operations/operations.js
+++ frontend/src/js/components/operations/operations.js
@@ -1,12 +1,13 @@
 import { Requests } from "../../helpers/requests";
 
 export class Operations {
-  constructor() {
-    console.log("income-and-expenses");
+  constructor(openNewRoute) {
+    this.openNewRoute = openNewRoute;
+
+    this.tableElement = document.getElementById("tbody");
 
     $("#startDate").datepicker({
       format: "mm.dd.yy",
-      // "language": 'ru',
       locale: "ru",
     });
 
@@ -26,6 +27,84 @@ export class Operations {
       );
     }
 
-    console.log(result);
+    this.showOperations(result);
+  }
+
+  showOperations(operations) {
+    operations.forEach((operation, index) => {
+      const trElement = document.createElement("tr");
+      trElement.setAttribute("data-operation", operation.id);
+
+      const tdNumberElement = document.createElement("th");
+      tdNumberElement.classList.add("text-center");
+      tdNumberElement.innerText = index + 1;
+
+      const tdOperationType = document.createElement("td");
+      tdOperationType.classList.add("text-center");
+      if (operation.type === "income") {
+        tdOperationType.innerText = "доход";
+        tdOperationType.classList.add("text-success");
+      } else {
+        tdOperationType.innerText = "расход";
+        tdOperationType.classList.add("text-danger");
+      }
+
+      const tdOperationCategory = document.createElement("td");
+      tdOperationCategory.classList.add("text-center");
+      //   tdOperationCategory.innerText = operation.category;
+
+      const tdAmountElement = document.createElement("td");
+      tdAmountElement.classList.add("text-center");
+      tdAmountElement.innerText = operation.amount + "$";
+
+      const tdDateElement = document.createElement("td");
+      tdDateElement.classList.add("text-center");
+      tdDateElement.innerText = operation.date.split("-").reverse().join(".");
+
+      const tdCommentElement = document.createElement("td");
+      tdCommentElement.classList.add("text-center");
+      tdCommentElement.innerText = operation.comment;
+
+      const tdActionsElement = document.createElement("td");
+      tdActionsElement.classList.add("d-flex", "gap-2", "justify-content-end");
+
+      const deleteIconElement = document.createElement("div");
+      deleteIconElement.classList.add("delete-action");
+      deleteIconElement.setAttribute("data-bs-toggle", "modal");
+      deleteIconElement.setAttribute("data-bs-target", "#modal");
+      deleteIconElement.setAttribute("role", "button");
+
+      const deleteIconImage = document.createElement("img");
+      deleteIconImage.setAttribute("src", "../../../assets/delete-action.svg");
+
+      deleteIconElement.appendChild(deleteIconImage);
+
+      const editIconElement = document.createElement("a");
+      editIconElement.setAttribute(
+        "href",
+        "/income-expense-edit?operation=" + operation.id
+      );
+      editIconElement.classList.add("edit-action", "text-black");
+
+      const editIconImage = document.createElement("img");
+      editIconImage.setAttribute("src", "../../../assets/edit-action.svg");
+
+      editIconElement.appendChild(editIconImage);
+
+      tdActionsElement.appendChild(deleteIconElement);
+      tdActionsElement.appendChild(editIconElement);
+
+      trElement.appendChild(tdNumberElement);
+      trElement.appendChild(tdOperationType);
+      trElement.appendChild(tdOperationCategory);
+      trElement.appendChild(tdAmountElement);
+      trElement.appendChild(tdDateElement);
+      trElement.appendChild(tdCommentElement);
+      trElement.appendChild(tdActionsElement);
+
+      this.tableElement.appendChild(trElement);
+    });
+
+    console.log(operations);
   }
 }
